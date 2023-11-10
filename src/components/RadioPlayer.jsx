0d393e44c25a9217a734a56855429e319f7f65ba
Isidro Marroquin <106924754+marroquin9953@users.noreply.github.com>
2023-11-10 14:31:25 -0600

    new

--- src/components/RadioPlayer.jsx
+++ src/components/RadioPlayer.jsx
@@ -3,29 +3,39 @@ import Hls from 'hls.js';
 
 import './RadioPlayer.css';
 
-const videoElement = document.getElementById('videoTag');
-const hls = new Hls();
-
 const RadioPlayer = ({ name, img, url, isPlaying, togglePlay }) => {
     useEffect(() => {
-        if (!url) {
-            return;
-        }
-        if (url.includes('.m3u8')) {
-            hls.loadSource(url);
-            hls.attachMedia(videoElement);
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                if (!isPlaying) {
-                    videoElement.play();
-                }
-            });
-        } else {
-            videoElement.src = url;
-            videoElement.play();
-        }
-    }, [url]);
+        const videoElement = document.getElementById('videoTag');
+        const hls = new Hls();
+
+        const initializePlayer = () => {
+            if (!url) {
+                return;
+            }
+
+            if (url.includes('.m3u8')) {
+                hls.loadSource(url);
+                hls.attachMedia(videoElement);
+                hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                    if (!isPlaying) {
+                        videoElement.play();
+                    }
+                });
+            } else {
+                videoElement.src = url;
+                videoElement.play();
+            }
+        };
+
+        initializePlayer();
+
+        return () => {
+            // Cleanup logic if needed
+        };
+    }, [url, isPlaying]);
 
     useEffect(() => {
+        const videoElement = document.getElementById('videoTag');
         isPlaying ? videoElement.play() : videoElement.pause();
     }, [isPlaying]);
 
@@ -36,6 +46,7 @@ const RadioPlayer = ({ name, img, url, isPlaying, togglePlay }) => {
             <button className="btn-player" onClick={togglePlay}>
                 <i className={isPlaying ? 'icon-pause' : 'icon-play'}></i>
             </button>
+            <video id="videoTag" controls></video>
         </div>
     ) : null;
 };
